d8afcac8bf91a8bce043a4071a7f9321d785b7bf
Alan Johnson <nalanj@fastmail.com>
2019-04-01 07:21:56 -0400

    Clarified Node Children docs a bit

--- node.go
+++ node.go
@@ -6,7 +6,9 @@ type Node interface {
 	// Type returns the type of the node
 	Type() NodeType
 
-	// Children returns the children of the node, if any
+	// Children returns the children of the node, if any. Maps have their
+	// children organized into pairs of keys and values, so, within a map
+	// Chidren()[even] is a key and Children()[odd] is a value.
 	Children() []Node
 
 	// Decorator returns the decorator for the node, or an empty string if
